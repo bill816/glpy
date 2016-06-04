c07e8f83a0c17790d0919e88f249c5bf1e4ea4d6
bill <scguoxiao@163.com>
2016-06-04 22:14:27 +0800

    get history data from tushare and insert to mongodb STOCK_TICK database tickdata table

--- src/gl.data/gl_tushare.py
+++ src/gl.data/gl_tushare.py
@@ -1,6 +1,13 @@
 import tushare as ts
+from pymongo import MongoClient
+import json
 
 if __name__ == '__main__':
-    print ts.get_hist_data('600848')
+    client = MongoClient('localhost', 27017)
+    df = ts.get_hist_data('000001')
+    print type(df)
+    db = client.get_database('STOCK_TICK')
+    print type(db)
+    db.tickdata.insert(json.loads(df.to_json(orient='records')))
     print 'hello'
     
