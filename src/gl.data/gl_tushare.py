e21a30d99abebb6788a12a166404749c3af4b7ad
bill <scguoxiao@163.com>
2016-06-07 07:12:18 +0800

    add some mongodb operation

--- src/gl.data/gl_tushare.py
+++ src/gl.data/gl_tushare.py
@@ -2,12 +2,42 @@ import tushare as ts
 from pymongo import MongoClient
 import json
 
+
+
 if __name__ == '__main__':
+    
+    
     client = MongoClient('localhost', 27017)
-    df = ts.get_hist_data('000001')
-    print type(df)
     db = client.get_database('STOCK_TICK')
-    print type(db)
-    db.tickdata.insert(json.loads(df.to_json(orient='records')))
+    
+    #for item in db["sh"].find():
+    #    print item
+    #sh_df = ts.get_h_data('sh', start='2009-01-01', end='2011-06-3')
+    #print type(sh_df)
+    #db.sh.insert(json.loads(sh_df.to_json(orient='records')))
+    '''
+    sh_df = ts.get_h_data('sh', start='2011-06-04', end='2013-06-3')
+    print type(sh_df)
+    db.sh.insert(json.loads(sh_df.to_json(orient='records')))
+    
+    sh_df = ts.get_h_data('sh', start='2013-06-04', end='2015-06-3')
+    print type(sh_df)
+    db.sh.insert(json.loads(sh_df.to_json(orient='records')))
+    '''
+    
+    sh_df = ts.get_hist_data('600848',ktype='W')
+    print sh_df
+    for item in json.loads(sh_df.to_json(orient='records')):
+        print item
+    #db['sh'].drop()
+    
+    #db.sh.insert(json.loads(sh_df.to_json()))
+    sh_df.to_json('d:/600848.json',orient='records')
+  #  print sh_df
+    print '------------------------------------------'
+    print '------------------------------------------'
+    print '------------------------------------------'
+  #  print sh_df.to_json
+    
     print 'hello'
     
