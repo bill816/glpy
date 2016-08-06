09eca45cf90259ddef2ba76ebdd964a100caf423
bill <scguoxiao@163.com>
2016-08-06 22:55:11 +0800

    have finished but the result is not correct , need to optimization

--- src/gl.math/gl_bayes_mail.py
+++ src/gl.math/gl_bayes_mail.py
@@ -21,11 +21,19 @@ __mail_spam_map = {}
 # 其中每个键对应的值是一个列表，列表中保存了该单词的次数
 __mail_ham_map = {}
 
-
+'''
 HAM_PATH = 'D:/workspace/git_project/email/train/ham/*.txt'
 SPAM_PATH = 'D:/workspace/git_project/email/train/spam/*.txt'
 HAM_TEST_PATH = 'D:/workspace/git_project/email/test/ham/*.txt'
 SPAM_TEST_PATH = 'D:/workspace/git_project/email/test/spam/*.txt'
+'''
+
+
+HAM_PATH = 'D:/work/git_project/email/train/ham/*.txt'
+SPAM_PATH = 'D:/work/git_project/email/train/spam/*.txt'
+
+HAM_TEST_PATH = 'D:/work/git_project/email/test/ham/*.txt'
+SPAM_TEST_PATH = 'D:/work/git_project/email/test/spam/*.txt'
 
 
 HAM_RESULT_PATH = "ham_result.pkl"
@@ -94,14 +102,14 @@ def start_test(path):
     #p(y|x)=p(x|y)*p(y)/p(x)
     #y垃圾邮件 x某个单词
     #p(y|x)：  当y邮件出现了x单词，这个邮件y是垃圾邮件的概率
-    #p(x|y): 当y为垃圾邮件时，x单词出现的概率
+    #p(x|y):  当y为垃圾邮件时，x单词出现的概率
     #p(y)为50%
     #p(x)=
     
     __mail_spam_map = read_train_from_file(SPAM_RESULT_PATH)
     __mail_ham_map = read_train_from_file(HAM_RESULT_PATH)
-    print __mail_spam_map
-    print __mail_ham_map
+    #print __mail_spam_map
+    #print __mail_ham_map
     
     #从文件里读取内容保存到map
     for mail_file in glob.glob(path):
@@ -112,6 +120,7 @@ def start_test(path):
             spam_count=0
             ham_count=0
             
+            #1 计算px
             try:
                 spam_count = __mail_spam_map[item]
             except KeyError:
@@ -121,17 +130,67 @@ def start_test(path):
             except KeyError:
                 ham_count = 0
                    
-            item_count = spam_count + ham_count
-            px = 0.0
-            if item_count > 0:
-                word_count = __mail_spam_map[TOTAL_OF_WORD] + __mail_ham_map[TOTAL_OF_WORD]
-                #print item
-                #print item_count
-                #print word_count
-                px =float(float(item_count)/float(word_count))
-            print px
-        break
+            item_count = spam_count + ham_count #+ test_map[item]
+            word_count = __mail_spam_map[TOTAL_OF_WORD] + __mail_ham_map[TOTAL_OF_WORD] #+ test_map[TOTAL_OF_WORD]
+            px = float(float(item_count)/float(word_count))
+            #print px
+            
+            #2 计算px|y=pxy/py
+            pxy = float(float(spam_count) / float(__mail_spam_map[TOTAL_OF_WORD]) )
+            
+            #3 py 默认为50%
+            #实际训练垃圾邮件3373个，正常邮件为 9035个
+            #py = 0.5
+            py = 3373.0/(3373.0+9035.0)
+            
+            #4 计算pyx
+            #p(y|x)=p(x|y)*p(y)/p(x)
+            pyx = 0
+            if px != 0:
+                pyx = float(py * pxy / px)
+            save_pyx(item,pyx)
+            
+            '''
+            if pyx > 1:
+                print item
+                print 'spam_count:' , spam_count
+                print 'ham_count:' , ham_count
+                print 'test_map:' , test_map[item]
+                print '__mail_spam_map:' , __mail_spam_map[TOTAL_OF_WORD]
+                print '__mail_ham_map:' , __mail_ham_map[TOTAL_OF_WORD]
+                print 'test_map:' , test_map[TOTAL_OF_WORD]
+                
+                print pyx
+                print py
+                print pxy
+                print px
+                '''
+        sorted_result = sorted(pyx_map.items(), key=lambda d: d[1],reverse=True)
+        count1 = 0.0
+        total_of_percent = 0
+        for item1 in sorted_result:
+            if count1 >= 15:
+                break
+            total_of_percent = total_of_percent + item1[1]
+            count1 = count1 + 1
+            print total_of_percent
+#        break
+    
+pyx_map = {}
+def save_pyx(word,pyx):
+    pyx_map[word] = pyx
+ 
+def sort_by_value(d):
+    items=d.items()
+    backitems=[[v[1],v[0]] for v in items]
+    backitems.sort()
+    return [ backitems[i][1] for i in range(0,len(backitems))] 
 
 if __name__ == '__main__':
     #start_train()
     start_test(SPAM_TEST_PATH)
+    #start_test(HAM_TEST_PATH)
+    print 'end of test'
+    
+    
+    
