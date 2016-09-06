63c1cde2d63caedb4506b038c327a2f8e648112c
guoxiao <guoxiao@xinguodu.com>
2016-09-06 10:34:03 +0800

    增加格子切图

--- src/gl.image/image_conver.py
+++ src/gl.image/image_conver.py
@@ -14,11 +14,37 @@ def convertjpg(jpgfile,outdir,width=720,height=1280):
     #new_img.save(os.path.join(outdir,os.path.basename(jpgfile)))
     newImg.save(os.path.join(outdir,os.path.basename(jpgfile)))
 
+def getSplitImage(jpgfile,outdir):
+    img = Image.open(jpgfile)
+    startX = 1
+    startY = 241
+    width= 157
+    height = 90
+    gapX = 3
+    gapY = 9
+#    box=(1,240,1+185,240+110)
+    for i in range(0, 15):
+        startX1 = startX+(width*(i%3)) + gapX*(i%3)
+        startY1 = startY+(height*(i/3)) + gapY*(i/3)
+        
+        
+        box=(startX1,startY1,startX1+width,startY1+height)
+        region = img.crop(box)
+#        region.show()
+        region.save(os.path.join(outdir,"num_" + str(i) + ".png" ))
+#        newImg = Image.new("RGBA",(width,height),(64,64,64))
+#        newImg.paste(region,(0,0,width,height))
+#    newImg.show()
+#    newImg.save(os.path.join(outdir,os.path.basename(jpgfile)))
+   
     
-for jpgfile in glob.glob("D:/yingshuang_android/*.jpg"):
+#for jpgfile in glob.glob("D:/yingshuang_android/*.jpg"):
     #convertjpg(jpgfile,"D:/yingshuang_android/new_file")
-    print jpgfile
+#    print jpgfile
 
-newImg = Image.new("RGBA",(300,300),(255,0,255))
-newImg.show()
-    
+#newImg = Image.new("RGBA",(300,300),(255,0,255))
+#newImg.show()
+
+
+
+getSplitImage("D:/work/n5/PCI/shouying.jpg","D:/images")
